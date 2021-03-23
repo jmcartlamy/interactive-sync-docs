8a995c48e4223b437dcdda033789437c6c39259f
jmcartlamy <contact@cartlamy.com>
2021-03-23 17:42:29 +0100

    feat(legals): add terms of use

---
+++ src/pages/terms-of-use/index.js
@@ -0,0 +1,175 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
+function Community() {
+    return (
+        <Layout title="Terms of use" description="Terms and conditions">
+            <main>
+                <div className="container margin-vert--xl col col--6 col--offset-3">
+                    <div class="row">
+                        <div class="col col--9 col--offset-1">
+                            <h1 className="hero__title">Terms & conditions</h1>
+                            <h2>Version 1.0</h2>
+                            <p>
+                                These Terms of Use are an Agreement between You, the User ("You")
+                                and Jean-Marie Cart-Lamy ("Interactive Sync"), which owns and
+                                operates the documentation and Twitch.tv extension, and various
+                                other online services (collectively, the "Services"). The Services
+                                are offered to you conditioned on your acceptance without
+                                modification of the terms, conditions, and notices contained herein,
+                                as well as any additional licenses which may be provided with
+                                particular downloadable software or services. Your use of the
+                                Services constitutes your agreement to all such terms, conditions,
+                                and notices. If you do not agree to any of these terms, please do
+                                not use the Services.
+                            </p>
+                            <h2>Links to Third Party Sites</h2>
+                            <p>
+                                The Services contain links to other sites or services (“Third Party
+                                Sites”), including but not limited to twitch.tv. The Third Party
+                                Sites are not under the control of Interactive Sync, and we are not
+                                responsible for the contents of any Third Party Site, including
+                                without limitation any link contained in a Third Party Site, any
+                                changes or updates to a Third Party Site, or for any information
+                                transmitted by Interactive Sync to a Third Party Site. The inclusion
+                                of any link to a Third Party Site does not imply endorsement by
+                                Interactive Sync of the site or any association with its operators.
+                            </p>
+                            <h2>No Unlawful or Prohibited Uses</h2>
+                            <p>
+                                As a condition of your use of the Services, you warrant that you
+                                will not use the Services for any purpose that is unlawful or
+                                prohibited by these terms, conditions, and notices. You agree that
+                                when using the Services, you will not:
+                            </p>
+                            <ul>
+                                <li>
+                                    Defame, abuse, harass, stalk, threaten or otherwise violate the
+                                    legal rights (such as rights of privacy and publicity) of
+                                    others. Publish, post, upload, distribute or disseminate any
+                                    inappropriate, profane, defamatory, infringing, obscene,
+                                    indecent or unlawful topic, name, material or information.
+                                </li>
+                                <li>
+                                    Upload files that contain software or other material protected
+                                    by intellectual property laws (or by rights of privacy of
+                                    publicity) unless you own or control the rights thereto or have
+                                    received all necessary consents.
+                                </li>
+                                <li>
+                                    Upload files that contain viruses, corrupted files, or any other
+                                    similar software or programs that may damage the operation of
+                                    another’s computer.
+                                </li>
+                                <li>Advertise or offer to sell or buy any goods or services.</li>
+                                <li>
+                                    Use the Services in any manner which could damage, disable,
+                                    overburden, or impair the Services or interfere with any other
+                                    party’s use and enjoyment of the Services
+                                </li>
+                                <li>
+                                    Violate any code of conduct or other guidelines which may be
+                                    applicable for any particular Service.
+                                </li>
+                                <li>
+                                    Obtain or attempt to obtain any materials or information through
+                                    any means not intentionally made available or provided for
+                                    through the Services by Interactive Sync.
+                                </li>
+                                <li>Violate any applicable laws or regulations.</li>
+                            </ul>
+                            <p>
+                                Interactive Sync is committed to ensuring that the Services are as
+                                useful and efficient as possible. For that reason, we reserve the
+                                right to make changes to the Services or to charge for its services,
+                                at any time and for any reason.
+                            </p>
+                            <h2>Monitoring of Communications</h2>
+                            <p>
+                                Interactive Sync has no obligation to monitor the Services. However,
+                                Interactive Sync reserves the right to review any communications
+                                transmitted using the Services and to remove or block any materials
+                                in its sole discretion, and/or to monitor for security purposes.
+                                Interactive Sync reserves the right at all times to disclose any
+                                information as necessary to satisfy any applicable law, regulation,
+                                legal process, or governmental request, or to edit, refuse to post,
+                                or to remove any information or materials, in whole or in part, in
+                                Interactive Sync’s sole discretion.
+                            </p>
+                            <h2>Submissions Provided to or Posted Using the Services</h2>{' '}
+                            <p>
+                                Interactive Sync does not claim ownership of the materials you
+                                provide, post, upload, input, or submit to the Services
+                                (collectively “Submission”). However, by posting, uploading,
+                                inputting, providing, or submitting your Submission you are granting
+                                Interactive Sync, its affiliated companies, and necessary
+                                sub-licensees, permission to use your Submission in connection with
+                                the operation of their businesses, including, without limitation,
+                                the rights to: copy, distribute, transmit, publicly display,
+                                publicly perform, reproduce, edit, translate and reformat your
+                                Submission. You grant Interactive Sync a perpetual, paid-up,
+                                irrevocable, worldwide, transferable license to use and create
+                                derivative works from any Submission which constitutes a suggestion
+                                for improvement to any Interactive Sync Services or services. No
+                                compensation will be paid with respect to the use of your
+                                Submission, as provided herein. Interactive Sync is under no
+                                obligation to post or use any Submission you may provide and may
+                                remove any Submission at any time in Interactive Sync’s sole
+                                discretion. By posting, uploading, inputting, providing or
+                                submitting your Submission you warrant and represent that you own or
+                                otherwise control all of the rights to your Submission as described
+                                in this section including, without limitation, all the rights
+                                necessary for you to provide, post, upload, input or submit the
+                                Submissions.
+                            </p>
+                            <h2>Liability disclaimer</h2>
+                            <p>
+                                With respect to Interactive Sync’s responsibility for your use of
+                                the Services, when you’re using the Services, it’s important to bear
+                                in mind that although we endeavour to ensure that it is updated and
+                                correct at all times, we do rely on third parties to provide
+                                information to us so that we can make it available to you.
+                                Interactive Sync accepts no liability for any loss, direct or
+                                indirect, you experience as a result of relying wholly on
+                                functionalities of Services.
+                            </p>
+                            <h2>Termination or Restriction of Access</h2>
+                            <p>
+                                Interactive Sync reserves the right, in its sole discretion, to
+                                terminate your access to the Services and the related services or
+                                any portion thereof at any time, without notice. In addition, your
+                                rights automatically terminate if You in any way breach any of these
+                                Terms.
+                            </p>
+                            <h2>Copyright</h2>
+                            <p>
+                                Unless otherwise indicated, all contents of the Services are:
+                                Copyright 2021 by Jean-Marie Cart-Lamy. All rights reserved.
+                            </p>
+                            <h2>Changes to this Terms & Conditions</h2>
+                            <p>
+                                Interactive Sync reserves the right to change the terms, conditions,
+                                and notices under which the Services are offered, including but not
+                                limited to the charges associated with the use of the Services. If
+                                we change these Terms in what we believe are a material way, we will
+                                provide reasonable notice to you.
+                            </p>
+                            <p>These terms and conditions are effective as of 2021-03-23</p>
+                            <h2>Contact Us</h2>
+                            <p>
+                                If you have any questions or suggestions about my Terms and
+                                Conditions, do not hesitate to contact me at:
+                                <br />
+                                interactive-sync[at]gmail.com.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </Layout>
+    );
+}
+
+export default Community;
